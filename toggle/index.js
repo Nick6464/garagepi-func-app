7bcd1af46c2ec7346beb8f26a73fabe7d8b8d732
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-10-04 15:50:09 +1300

    Complete Overhaul of JWT verification

--- toggle/index.js
+++ toggle/index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const passport = require('passport');
 const auth = require('../auth'); // Adjust the path as needed
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const {
@@ -14,93 +15,86 @@ const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = async function (context, req) {
   try {
-    // Initialize Passport
-    passport.initialize()(req, context.res, () => {});
+    const publicKey = await fetchPublicKey();
 
-    // Check if user is authenticated
-    // Replace this check with your bearer strategy middleware
-    auth.pAuth(req, context.res, async () => {
-      // The request is authenticated
-      console.log('User Authenticated:', req.isAuthenticated());
+    const decodedToken = verifyJwtToken(
+      req.headers.authorization.split(' ')[1],
+      publicKey
+    );
 
-      // You can access the authenticated user and other details from req.user, req.tenant, req.tenantUser
+    if (!decodedToken) {
+      context.res = {
+        status: 401,
+        body: 'Unauthorized: Token validation failed',
+      };
+      return;
+    }
 
-      const authHeader = req.headers.authorization;
-      if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        // Handle the case where the token is missing or not in the correct format
+    try {
+      // Check the aud claim
+      if (
+        decodedToken.aud !== AZURE_AD_CLIENT_ID &&
+        decodedToken.aud !== AZURE_AD_CLIENT_ID_DEV
+      ) {
+        console.log('Invalid audience');
         context.res = {
           status: 401,
-          body: 'Unauthorized: Token missing or in the wrong format',
+          body: 'Unauthorized: Token has incorrect audience',
         };
         return;
       }
 
-      // Extract the token part from the header
-      const token = authHeader.split(' ')[1];
-
-      try {
-        // Check the aud claim
-        if (
-          decodedToken.aud !== AZURE_AD_CLIENT_ID &&
-          decodedToken.aud !== AZURE_AD_CLIENT_ID_DEV
-        ) {
-          context.res = {
-            status: 401,
-            body: 'Unauthorized: Token has incorrect audience',
-          };
-          return;
-        }
-
-        // Check the tid claim
-        if (decodedToken.tid !== AZURE_AD_TENANT_ID) {
-          context.res = {
-            status: 401,
-            body: 'Unauthorized: Token has incorrect tenant',
-          };
-          return;
-        }
-
-        // Check if the token is expired
-        const currentTimestamp = Math.floor(Date.now() / 1000);
-        if (decodedToken.exp && currentTimestamp >= decodedToken.exp) {
-          context.res = {
-            status: 401,
-            body: 'Unauthorized: Token has expired',
-          };
-          return;
-        }
-
-        console.log('Decoded token:', decodedToken);
-
-        // Check if the token contains the required role
-        const requiredRole = 'toggle';
-        const userRoles = decodedToken.roles || [];
-
-        if (!userRoles.includes(requiredRole)) {
-          context.res = {
-            status: 403, // Forbidden
-            body: 'User does not have the required role',
-          };
-          return;
-        }
-
-        // Make an HTTP request to the Raspberry Pi
-        // const response = await axios.get(`${RASPBERRY_PI_ENDPOINT}/toggle`);
-
-        // Return a success response to the client
+      // Check the tid claim
+      if (decodedToken.tid !== AZURE_AD_TENANT_ID) {
+        console.log('Invalid tenant');
         context.res = {
-          status: 200,
-          body: 'Toggle request sent successfully',
+          status: 401,
+          body: 'Unauthorized: Token has incorrect tenant',
         };
-      } catch (error) {
-        console.error('Error:', error);
-
-        context.res = {
-          status: 401, // Unauthorized
-          body: 'Unauthorized: Token validation failed',
-        };
+        return;
       }
-    });
+
+      // Check if the token is expired
+      const currentTimestamp = Math.floor(Date.now() / 1000);
+      if (decodedToken.exp && currentTimestamp >= decodedToken.exp) {
+        console.log('Token has expired');
+        context.res = {
+          status: 401,
+          body: 'Unauthorized: Token has expired',
+        };
+        return;
+      }
+
+      // Check if the token contains the required role
+      const requiredRole = 'toggle';
+      const userRoles = decodedToken.roles || [];
+
+      if (!userRoles.includes(requiredRole)) {
+        console.log('User does not have the required role');
+        context.res = {
+          status: 403, // Forbidden
+          body: 'User does not have the required role',
+        };
+        return;
+      }
+
+      // Make an HTTP request to the Raspberry Pi
+      // const response = await axios.get(`${RASPBERRY_PI_ENDPOINT}/toggle`);
+
+      // Return a success response to the client
+      console.log('Toggle request sent successfully');
+      context.res = {
+        status: 200,
+        body: 'Toggle request sent successfully',
+      };
+    } catch (error) {
+      console.error('Error:', error);
+
+      context.res = {
+        status: 401, // Unauthorized
+        body: 'Unauthorized: Token validation failed',
+      };
+    }
   } catch (error) {
     console.error('Error:', error);
 
@@ -110,3 +104,38 @@ module.exports = async function (context, req) {
     };
   }
 };
+
+// Function to verify a JWT token
+function verifyJwtToken(token, publicKey) {
+  try {
+    const decoded = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+    // If the token is valid, 'decoded' will contain the token payload
+    console.log('JWT verification succeeded');
+    // console.log('JWT verification succeeded:', decoded);
+    return decoded;
+  } catch (error) {
+    // If the token is invalid or has expired, an error will be thrown
+    console.error('JWT verification failed:', error.message);
+    return null;
+  }
+}
+
+async function fetchPublicKey() {
+  const opt = (
+    await axios.get(
+      `https://login.microsoftonline.com/${AZURE_AD_TENANT_ID}/v2.0/.well-known/openid-configuration`
+    )
+  ).data;
+
+  // Use the jwks_uri to get the signing keys
+  const jwksResponse = await axios.get(opt.jwks_uri);
+  const jwks = jwksResponse.data;
+
+  // Extract the X.509 certificate
+  const x5c = jwks.keys[0].x5c[0];
+
+  // Parse the X.509 certificate into a usable public key format (PEM)
+  const publicKeyPem = `-----BEGIN CERTIFICATE-----\n${x5c}\n-----END CERTIFICATE-----`;
+
+  return publicKeyPem;
+}
