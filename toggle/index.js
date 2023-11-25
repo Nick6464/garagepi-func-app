5deae510b312c32fab5108307f0a9d7ede6a2ffa
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-11-25 20:16:47 +1300

    Fixed JWT verifying and made auth flow as I wanted it

--- toggle/index.js
+++ toggle/index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
 require('dotenv').config();
 
 const { AZURE_AD_TENANT_ID, RASPBERRY_PI_ENDPOINT } = process.env;
@@ -33,27 +34,23 @@ module.exports = async function (context, req) {
         };
         return;
       }
-
-      // Check if the token contains the required role
       const requiredRole = 'toggle';
       const userRoles = decodedToken.roles || [];
 
       if (!userRoles.includes(requiredRole)) {
         console.log('User does not have the required role');
         context.res = {
-          status: 403, // Forbidden
+          status: 403, 
           body: 'User does not have the required role',
         };
         return;
       }
-
       // Make an HTTP request to the Raspberry Pi
       let resp = await axios.get(`${RASPBERRY_PI_ENDPOINT}/press`, {
         headers: {
           Authorization: req.headers.authorization,
         },
       });
-
       // Return a success response to the client
       console.log('Toggle request sent successfully');
       context.res = {
@@ -77,45 +74,40 @@ module.exports = async function (context, req) {
     };
   }
 };
-
-// Function to verify a JWT token
 async function verifyJwtToken(token) {
-  let publicKey;
   try {
-    publicKey = await fetchPublicKey();
-  } catch (error) {
-    console.error('Error fetching public key:', error);
-  }
+    const jwksUri = `https://login.microsoftonline.com/${AZURE_AD_TENANT_ID}/discovery/v2.0/keys`;
 
-  try {
-    const decoded = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
-    // If the token is valid, 'decoded' will contain the token payload
+    const signingKey = await getSigningKey(
+      jwksUri,
+      jwt.decode(token, { complete: true }).header.kid
+    );
+    const decoded = jwt.verify(token, signingKey, { algorithms: ['RS256'] });
     console.log('JWT verification succeeded');
-    // console.log('JWT verification succeeded:', decoded);
     return decoded;
   } catch (error) {
-    // If the token is invalid or has expired, an error will be thrown
     console.error('JWT verification failed:', error.message);
     return null;
   }
 }
 
-async function fetchPublicKey() {
-  const opt = (
-    await axios.get(
-      `https://login.microsoftonline.com/${AZURE_AD_TENANT_ID}/v2.0/.well-known/openid-configuration`
-    )
-  ).data;
-
-  // Use the jwks_uri to get the signing keys
-  const jwksResponse = await axios.get(opt.jwks_uri);
-  const jwks = jwksResponse.data;
-
-  // Extract the X.509 certificate
-  const x5c = jwks.keys[0].x5c[0];
-
-  // Parse the X.509 certificate into a usable public key format (PEM)
-  const publicKeyPem = `-----BEGIN CERTIFICATE-----\n${x5c}\n-----END CERTIFICATE-----`;
-
-  return publicKeyPem;
+async function getSigningKey(jwksUri, kid) {
+  try {
+    const client = jwksClient({
+      jwksUri,
+    });
+    const key = await new Promise((resolve, reject) => {
+      client.getSigningKey(kid, (err, key) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(key.getPublicKey());
+        }
+      });
+    });
+    return key;
+  } catch (error) {
+    console.error('Error fetching signing key:', error);
+    throw error;
+  }
 }
